cdee316d8e741bae643a0160bfd76152ac907cb8
liulingyu <liulingyu@lechebang.com>
2019-10-15 14:56:33 +0800

    docs(*):template类型字符类型转换AST语法树过程解析

--- vue/src/compiler/parser/html-parser.js
+++ vue/src/compiler/parser/html-parser.js
@@ -9,9 +9,16 @@
  * http://erik.eae.net/simplehtmlparser/simplehtmlparser.js
  */
 
-import { makeMap, no } from 'shared/util'
-import { isNonPhrasingTag } from 'web/compiler/util'
-import { unicodeRegExp } from 'core/util/lang'
+import {
+  makeMap,
+  no
+} from 'shared/util'
+import {
+  isNonPhrasingTag
+} from 'web/compiler/util'
+import {
+  unicodeRegExp
+} from 'core/util/lang'
 
 // Regular Expressions for parsing tags and attributes
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
@@ -46,37 +53,44 @@ const encodedAttrWithNewLines = /&(?:lt|gt|quot|amp|#39|#10|#9);/g
 const isIgnoreNewlineTag = makeMap('pre,textarea', true)
 const shouldIgnoreFirstNewline = (tag, html) => tag && isIgnoreNewlineTag(tag) && html[0] === '\n'
 
-function decodeAttr (value, shouldDecodeNewlines) {
+function decodeAttr(value, shouldDecodeNewlines) {
   const re = shouldDecodeNewlines ? encodedAttrWithNewLines : encodedAttr
   return value.replace(re, match => decodingMap[match])
 }
 
-export function parseHTML (html, options) {
+// 解析html，生成AST语法树
+export function parseHTML(html, options) {
   const stack = []
   const expectHTML = options.expectHTML
   const isUnaryTag = options.isUnaryTag || no
   const canBeLeftOpenTag = options.canBeLeftOpenTag || no
   let index = 0
-  let last, lastTag
+  let last, lastTag // 栈内最后一个标签
   while (html) {
     last = html
     // Make sure we're not in a plaintext content element like script/style
     if (!lastTag || !isPlainTextElement(lastTag)) {
       let textEnd = html.indexOf('<')
+      // 匹配到开始标签
       if (textEnd === 0) {
         // Comment:
+        // 注释部分
         if (comment.test(html)) {
           const commentEnd = html.indexOf('-->')
-
+          // index大于0说明有注释内容
           if (commentEnd >= 0) {
+            // 保留注释内容的选项，默认是不保留
             if (options.shouldKeepComment) {
+              // 注释节点生成ASTnode插入
               options.comment(html.substring(4, commentEnd), index, index + commentEnd + 3)
             }
+            // 推迟
             advance(commentEnd + 3)
             continue
           }
         }
 
+        // ie浏览器的加载样式节点的注释: <!--[if !IE]>--><link href="non-ie.css" rel="stylesheet"><!--<![endif]-->
         // http://en.wikipedia.org/wiki/Conditional_comment#Downlevel-revealed_conditional_comment
         if (conditionalComment.test(html)) {
           const conditionalEnd = html.indexOf(']>')
@@ -87,26 +101,33 @@ export function parseHTML (html, options) {
           }
         }
 
+        // 文档类型申明相关
         // Doctype:
         const doctypeMatch = html.match(doctype)
         if (doctypeMatch) {
+          // 直接截取掉不作处理
           advance(doctypeMatch[0].length)
           continue
         }
 
+        // 结束标签
         // End tag:
         const endTagMatch = html.match(endTag)
         if (endTagMatch) {
           const curIndex = index
+          // 截取标签长度字符
           advance(endTagMatch[0].length)
+          // 对结束标签做处理
           parseEndTag(endTagMatch[1], curIndex, index)
           continue
         }
 
+        // 开始标签处理，用来收集标签内的全部属性
         // Start tag:
         const startTagMatch = parseStartTag()
         if (startTagMatch) {
           handleStartTag(startTagMatch)
+          // pre、textArea标签特殊处理
           if (shouldIgnoreFirstNewline(startTagMatch.tagName, html)) {
             advance(1)
           }
@@ -116,8 +137,11 @@ export function parseHTML (html, options) {
 
       let text, rest, next
       if (textEnd >= 0) {
+        // 如果<标签前任然有文本，截取文本之后的内容
         rest = html.slice(textEnd)
+        // 这里循环匹配结束后，就会textEnd就会指向下一个作为左标签的<
         while (
+          // 非结束标签、非开始标签、非注释，则<符号是作为文本内容的
           !endTag.test(rest) &&
           !startTagOpen.test(rest) &&
           !comment.test(rest) &&
@@ -129,21 +153,24 @@ export function parseHTML (html, options) {
           textEnd += next
           rest = html.slice(textEnd)
         }
+        // 左标签前的文本内容
         text = html.substring(0, textEnd)
       }
-
+      // 没有左标签，纯文本
       if (textEnd < 0) {
         text = html
       }
-
+      // html推文本的长度
       if (text) {
         advance(text.length)
       }
-
+      // 对文本内容进行AST格式化处理
       if (options.chars && text) {
         options.chars(text, index - text.length, index)
       }
-    } else {
+    }
+    //  处理是script,style,textarea
+    else {
       let endTagLength = 0
       const stackedTag = lastTag.toLowerCase()
       const reStackedTag = reCache[stackedTag] || (reCache[stackedTag] = new RegExp('([\\s\\S]*?)(</' + stackedTag + '[^>]*>)', 'i'))
@@ -154,6 +181,7 @@ export function parseHTML (html, options) {
             .replace(/<!\--([\s\S]*?)-->/g, '$1') // #7298
             .replace(/<!\[CDATA\[([\s\S]*?)]]>/g, '$1')
         }
+        //匹配tag标签是pre,textarea，并且第二个参数的第一个字符是回车键
         if (shouldIgnoreFirstNewline(stackedTag, text)) {
           text = text.slice(1)
         }
@@ -170,7 +198,9 @@ export function parseHTML (html, options) {
     if (html === last) {
       options.chars && options.chars(html)
       if (process.env.NODE_ENV !== 'production' && !stack.length && options.warn) {
-        options.warn(`Mal-formatted tag at end of template: "${html}"`, { start: index + html.length })
+        options.warn(`Mal-formatted tag at end of template: "${html}"`, {
+          start: index + html.length
+        })
       }
       break
     }
@@ -179,12 +209,13 @@ export function parseHTML (html, options) {
   // Clean up any remaining tags
   parseEndTag()
 
-  function advance (n) {
+  function advance(n) {
     index += n
     html = html.substring(n)
   }
 
-  function parseStartTag () {
+  // 处理开始标签
+  function parseStartTag() {
     const start = html.match(startTagOpen)
     if (start) {
       const match = {
@@ -192,46 +223,61 @@ export function parseHTML (html, options) {
         attrs: [],
         start: index
       }
+      // 截取
       advance(start[0].length)
+      // end为开始标签的末尾>
       let end, attr
+      // 若下个标签不是>，并且能匹配到属性
       while (!(end = html.match(startTagClose)) && (attr = html.match(dynamicArgAttribute) || html.match(attribute))) {
+        // 下个属性的索引
         attr.start = index
+        // 截取属性长度
         advance(attr[0].length)
         attr.end = index
+        // 属性推入数组
         match.attrs.push(attr)
       }
+      // 开始标签匹配结束
       if (end) {
         match.unarySlash = end[1]
         advance(end[0].length)
         match.end = index
+        // 这个标签全部内容
         return match
       }
     }
   }
 
-  function handleStartTag (match) {
-    const tagName = match.tagName
-    const unarySlash = match.unarySlash
-
+  function handleStartTag(match) {
+    const tagName = match.tagName //开始标签名称
+    const unarySlash = match.unarySlash //如果是/>标签 则unarySlash 是/。 如果是>标签 则unarySlash 是空
+    // html
     if (expectHTML) {
+      // 判断栈内最后一个标签是否是p(父标签是否是p)，并且自己是双标签
       if (lastTag === 'p' && isNonPhrasingTag(tagName)) {
+        // 最后一个p元素出栈
         parseEndTag(lastTag)
       }
+      //判断标签是否是 'colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr,source'并且上个标签和当前匹配的标签相同
       if (canBeLeftOpenTag(tagName) && lastTag === tagName) {
+        // 同名标签出栈
         parseEndTag(tagName)
       }
     }
 
+    // 单标签
     const unary = isUnaryTag(tagName) || !!unarySlash
 
     const l = match.attrs.length
     const attrs = new Array(l)
+    // 循环匹配收集的属性数组
     for (let i = 0; i < l; i++) {
       const args = match.attrs[i]
       const value = args[3] || args[4] || args[5] || ''
-      const shouldDecodeNewlines = tagName === 'a' && args[1] === 'href'
-        ? options.shouldDecodeNewlinesForHref
-        : options.shouldDecodeNewlines
+      // a标签的处理
+      const shouldDecodeNewlines = tagName === 'a' && args[1] === 'href' ?
+        options.shouldDecodeNewlinesForHref :
+        options.shouldDecodeNewlines
       attrs[i] = {
         name: args[1],
         value: decodeAttr(value, shouldDecodeNewlines)
@@ -242,8 +288,16 @@ export function parseHTML (html, options) {
       }
     }
 
+    // 非单标签直接入栈
     if (!unary) {
-      stack.push({ tag: tagName, lowerCasedTag: tagName.toLowerCase(), attrs: attrs, start: match.start, end: match.end })
+      // 这里的stack是parseHtml函数的局部栈，和index下面的全局栈基本相同
+      stack.push({
+        tag: tagName,
+        lowerCasedTag: tagName.toLowerCase(),
+        attrs: attrs,
+        start: match.start,
+        end: match.end
+      })
       lastTag = tagName
     }
 
@@ -252,14 +306,16 @@ export function parseHTML (html, options) {
     }
   }
 
-  function parseEndTag (tagName, start, end) {
+  function parseEndTag(tagName, start, end) {
     let pos, lowerCasedTagName
     if (start == null) start = index
     if (end == null) end = index
 
     // Find the closest opened tag of the same type
     if (tagName) {
+      // 全部转小写
       lowerCasedTagName = tagName.toLowerCase()
+      // 循环栈匹配栈内上一个同名的标签
       for (pos = stack.length - 1; pos >= 0; pos--) {
         if (stack[pos].lowerCasedTag === lowerCasedTagName) {
           break
@@ -267,22 +323,28 @@ export function parseHTML (html, options) {
       }
     } else {
       // If no tag name is provided, clean shop
+      // 无匹配结果
       pos = 0
     }
-
+    // 有匹配结果
     if (pos >= 0) {
       // Close all the open elements, up the stack
+      // 这里会把匹配到的标签，包括标签的子标签都执行end方法（大部分情况下，匹配的标签应该是最后一个标签，但估计是自闭标签会有影响）
       for (let i = stack.length - 1; i >= pos; i--) {
+        // 这里若最后一个节点不是匹配节点，发出警告
         if (process.env.NODE_ENV !== 'production' &&
           (i > pos || !tagName) &&
           options.warn
         ) {
           options.warn(
-            `tag <${stack[i].tag}> has no matching end tag.`,
-            { start: stack[i].start, end: stack[i].end }
+            `tag <${stack[i].tag}> has no matching end tag.`, {
+              start: stack[i].start,
+              end: stack[i].end
+            }
           )
         }
         if (options.end) {
+          // 匹配标签出栈，修改指针指向
           options.end(stack[i].tag, start, end)
         }
       }
@@ -290,11 +352,16 @@ export function parseHTML (html, options) {
       // Remove the open elements from the stack
       stack.length = pos
       lastTag = pos && stack[pos - 1].tag
-    } else if (lowerCasedTagName === 'br') {
+    }
+    // 无匹配结果后，若标签为</br>
+    else if (lowerCasedTagName === 'br') {
       if (options.start) {
+        // 生成节点推入栈中，经过测试</br>和<br>同样处理换行
         options.start(tagName, [], true, start, end)
       }
-    } else if (lowerCasedTagName === 'p') {
+    }
+    // p标签插入指针子节点中
+    else if (lowerCasedTagName === 'p') {
       if (options.start) {
         options.start(tagName, [], false, start, end)
       }
--- vue/src/compiler/parser/text-parser.js
+++ vue/src/compiler/parser/text-parser.js
@@ -1,7 +1,11 @@
 /* @flow */
 
-import { cached } from 'shared/util'
-import { parseFilters } from './filter-parser'
+import {
+  cached
+} from 'shared/util'
+import {
+  parseFilters
+} from './filter-parser'
 
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 const regexEscapeRE = /[-.*+?^${}()|[\]\/\\]/g
@@ -14,34 +18,46 @@ const buildRegex = cached(delimiters => {
 
 type TextParseResult = {
   expression: string,
-  tokens: Array<string | { '@binding': string }>
+  tokens: Array < string | {
+    '@binding': string
+  } >
 }
 
-export function parseText (
+// 文本类型解析，返回整合的结果
+export function parseText(
   text: string,
-  delimiters?: [string, string]
+  delimiters ? : [string, string]
 ): TextParseResult | void {
+  // 纯文本类型格式化
   const tagRE = delimiters ? buildRegex(delimiters) : defaultTagRE
   if (!tagRE.test(text)) {
     return
   }
+  // tokens用来收集字符
   const tokens = []
   const rawTokens = []
   let lastIndex = tagRE.lastIndex = 0
   let match, index, tokenValue
+  // 循环匹配文本
   while ((match = tagRE.exec(text))) {
     index = match.index
     // push text token
+    // 起始位置不是标签，说明开始有纯文本字符串
     if (index > lastIndex) {
       rawTokens.push(tokenValue = text.slice(lastIndex, index))
       tokens.push(JSON.stringify(tokenValue))
     }
+
     // tag token
+    // 过滤解析{{}}中的内容，按照_s(exp)的方式插入tokens
     const exp = parseFilters(match[1].trim())
     tokens.push(`_s(${exp})`)
-    rawTokens.push({ '@binding': exp })
+    rawTokens.push({
+      '@binding': exp
+    })
     lastIndex = index + match[0].length
   }
+  // 匹配末尾阶段剩余的纯文本类型字符串
   if (lastIndex < text.length) {
     rawTokens.push(tokenValue = text.slice(lastIndex))
     tokens.push(JSON.stringify(tokenValue))
